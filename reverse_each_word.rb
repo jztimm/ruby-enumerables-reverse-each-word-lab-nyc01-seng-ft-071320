b2b22c9af6b5621785d29e9d3195474db42686a5
Jzavier Timm <jztimm@gmail.com>
2020-07-09 16:28:26 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,22 @@
 def reverse_each_word(str)
   str_arr = str.split(" ")
-  reverse_arr = []
-  str.each do |word|
-    reverse_arr << str_arr[word].reverse
+  reverse_arr = ""
+
+  str_arr.each do |word|
+    reverse.push(str_arr[word].reverse)
   end
+  reverse_arr.join(" ")
   reverse_arr
 end
+
+
+
+# js solve -
+
+# split the str
+# loop through each iteration of new_arr and use the reverse method to reverse each str
+# store reversed str into reverse_arr
+# join together with .join(" ")
+
+
+

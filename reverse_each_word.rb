3d927edd4709b0f5c0fd3581c4ceae86e4d8463b
Jzavier Timm <jztimm@gmail.com>
2020-07-09 16:56:25 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,17 +1,17 @@
 require 'pry'
 
-# def reverse_each_word(str)
-#   str_arr = str.split(" ")
-#   reverse_arr = []
+def reverse_each_word(str)
+  str_arr = str.split(" ")
+  reverse_arr = []
 
-#   str_arr.each do |word|
-#     reverse_word = word.reverse!
-#     reverse_arr << reverse_word
-#   end
-#   reverse_arr.join(" ")
-# end
+  str_arr.each do |word|
+    reverse_word = word.reverse!
+    reverse_arr << reverse_word
+  end
+  reverse_arr.join(" ")
+end
 
-def reverse_each_word_collect(string)
+def reverse_each_word(string)
   new_arr = string.split(" ")
   return_arr = []
   

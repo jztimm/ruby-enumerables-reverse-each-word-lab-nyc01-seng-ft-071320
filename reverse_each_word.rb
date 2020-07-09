71bf1b903c2e5a167237adbda4ac819fa01444df
Jzavier Timm <jztimm@gmail.com>
2020-07-09 16:49:48 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,13 +2,13 @@ require 'pry'
 
 def reverse_each_word(str)
   str_arr = str.split(" ")
-  reverse_arr = ""
+  reverse_arr = []
 
   str_arr.each do |word|
     reverse_word = word.reverse!
-    reverse_arr << reverse_word.concat(" ")
+    reverse_arr << reverse_word
   end
-  reverse_arr
+  reverse_arr.join(" ")
 end
 
 

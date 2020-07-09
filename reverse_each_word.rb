f6667bbfdbc35254c89d36249438a3f9c98b3116
Jzavier Timm <jztimm@gmail.com>
2020-07-09 16:35:14 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,7 +4,7 @@ def reverse_each_word(str)
 
   str_arr.each do |word|
     reverse_word = word.reverse!
-    reverse_arr << (reverse_word)
+    reverse_arr << reverse_word
   end
   reverse_arr.join(" ")
   #binding.pry
